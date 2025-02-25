62d7b1f5b5d7c47bec4cc2dd8f22cfb23c3fe44d
Аксинья <aksiniakhariuzova@gmail.com>
2025-02-25 17:54:59 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 
 int add(int a, int b);
 int subtract(int a, int b);
+int multiply(int a, int b);
 
 int main() {
     int a = 5, b = 3;
     std::cout << "Сложение: " << add(a, b) << std::endl;
     std::cout << "Вычитание: " << subtract(a, b) << std::endl;
+    std::cout << "Умножение: " << multiply(a, b) << std::endl;
 }
